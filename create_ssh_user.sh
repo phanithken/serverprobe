e6665e0c3802158e159f160ac4fbf154f1ebb08f
phanithken <kenphanith@gmail.com>
2025-01-03 15:39:16 +0700

    feat: add lsyncd process

--- create_ssh_user.sh
+++ create_ssh_user.sh
@@ -1,29 +1,64 @@
 #!/bin/bash
-# ./create_ssh_user.sh username
+# ./create_ssh_user.sh username domain
 
-if [ $# -eq 0 ]; then
-  echo "Please provide a username as an argument."
+if [ $# -ne 2 ]; then
+  echo "Usage ./create_ssh_user.sh username"
   exit 1
 fi
 
 USERNAME=$1
-sudo adduser --disabled-password --gecos "" $USERNAME
+DOMAIN=$2
 
-sudo mkdir -p /home/$USERNAME/.ssh
-sudo chmod 700 /home/$USERNAME/.ssh
+if id "$USERNAME" &>/dev/null; then
+  echo "User $USERNAME already exists. Skipping user creation."
+  exit 1
+else
+  sudo adduser --disabled-password --gecos "" $USERNAME
 
-sudo touch /home/$USERNAME/.ssh/authorized_keys
-sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys
+  sudo mkdir -p /home/$USERNAME/.ssh
+  sudo chmod 700 /home/$USERNAME/.ssh
 
-sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+  sudo touch /home/$USERNAME/.ssh/authorized_keys
+  sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys
 
-echo "User $USERNAME created successfully."
-echo "Public key can be added to /home/$USERNAME/.ssh/authorized_keys"
+  sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
-sudo usermod -aG sudo $USERNAME
+  echo "User $USERNAME created successfully."
+  echo "Public key can be added to /home/$USERNAME/.ssh/authorized_keys"
 
-read -p "Enable passwordless sudo for $USERNAME? (y/n): " PASSWORDLESS_SUDO
-if [ $PASSWORDLESS_SUDO = "y" ]; then
-  echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USERNAME
-  echo "Passwordless sudo enabled for $USERNAME."
+  sudo usermod -aG sudo $USERNAME
+
+  read -p "Enable passwordless sudo for $USERNAME? (y/n): " PASSWORDLESS_SUDO
+  if [ $PASSWORDLESS_SUDO = "y" ]; then
+    echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USERNAME
+    echo "Passwordless sudo enabled for $USERNAME."
+  fi
 fi
+
+if ! command -v lsyncd &> /dev/null; then
+  echo "Installing lsyncd..."
+  sudo apt update && sudo apt install lsyncd -y
+fi
+
+LSYNCD_CONF="/etc/lsyncd/lsyncd_$USERNAME.conf"
+sudo tee $LSYNCD_CONF > /dev/null <<EOF
+settings {
+  logfile = "/var/log/lsyncd/lsyncd_$USERNAME.log",
+  statusFile = "/var/log/lsyncd/lsyncd_$USERNAME.status",
+}
+sync {
+  default.rsync,
+  source = "/home/$USERNAME/$DOMAIN",
+  target = "/var/www/$DOMAIN",
+  rsync = {
+    archive = true,
+    compress = true,
+    whole_file = false,
+  }
+}
+EOF
+
+sudo systemctl enable lsyncd
+sudo systemctl restart lsyncd
+
+echo "lsyncd configuration created for $USERNAME."
